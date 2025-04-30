cf956449659db811e390fb8f2c5fefcc54197a94
Матвей Яблонский <matveyski25@gmail.com>
2025-04-30 09:21:47 +0300

    First

--- ComplexSequentialNeuronet/ComplexSequentialNeuronet.cpp
+++ ComplexSequentialNeuronet/ComplexSequentialNeuronet.cpp
@@ -53,160 +53,196 @@ namespace FunctionsAtivate {
 
 class SimpleSNT {
 public:
-	struct weights_and_displacement_Neuron {
-		long double weight_ForgetGate_for_HiddenState, weight_InputGate_for_HiddenState, weight_CandidateGate_for_HiddenState, weight_OutputGate_for_HiddenState;
-		long double weight_ForgetGate_for_InputState, weight_InputGate_for_InputState, weight_CandidateGate_for_InputState, weight_OutputGate_for_InputState;
-		long double displacement_ForgetState, displacement_InputState, displacement_CandidateState, displacement_OutputState;
-		weights_and_displacement_Neuron(const std::vector <long double> weights, const std::vector <long double> displacements) {
-			if(weights.size() > 0 && displacements.size() > 0) {
-				std::vector <long double> weights_ = weights;
-				if (weights.size() < 8) {
-					weights_ = weights;
-					weights_.resize(8);
-					for (short i = weights.size() - 1; i < 8; i++) {
-						weights_[i] = 0;
-					}
+	SimpleSNT(size_t NumberNeurons_ = 1, const std::vector <long double> weights = {0, 0, 0, 0, 0, 0, 0, 0}, const std::vector <long double> displacements = { 0, 0, 0, 0}) {
+		if (weights.size() > 0 && displacements.size() > 0) {
+			std::vector <long double> weights_ = weights;
+			if (weights.size() < 8) {
+				weights_ = weights;
+				weights_.resize(8);
+				for (short i = weights.size() - 1; i < 8; i++) {
+					weights_[i] = 0;
 				}
-				else {
-					weights_ = weights;
-				}
-
-				std::vector <long double> displacements_ = displacements;
-				if (weights.size() < 4) {
-					displacements_ = displacements;
-					displacements_.resize(4);
-					for (short i = displacements.size() - 1; i < 4; i++) {
-						displacements_[i] = 0.5;
-					}
-				}
-				else {
-					displacements_ = displacements;
-				}
-
-				this->weight_ForgetGate_for_HiddenState = weights_[0];
-				this->weight_InputGate_for_HiddenState = weights_[1];
-				this->weight_CandidateGate_for_HiddenState = weights_[2];
-				this->weight_OutputGate_for_HiddenState = weights_[3];
-
-				this->weight_ForgetGate_for_InputState = weights_[4];
-				this->weight_InputGate_for_InputState = weights_[5];
-				this->weight_CandidateGate_for_InputState = weights_[6];
-				this->weight_OutputGate_for_InputState = weights_[7];
-
-				this->displacement_ForgetState = displacements_[0];
-				this->displacement_InputState = displacements_[1];
-				this->displacement_CandidateState = displacements_[2];
-				this->displacement_OutputState = displacements_[3];
 			}
-		}
-		weights_and_displacement_Neuron(const std::vector <long double> weights_and_displacements) {
-			if(weights_and_displacements.size() > 0){
-				std::vector <long double> weights_and_displacements_ = weights_and_displacements;
-				if (weights_and_displacements.size() < 12) {
-					weights_and_displacements_ = weights_and_displacements;
-					weights_and_displacements_.resize(12);
-					for (short i = weights_and_displacements.size() - 1; i < 8; i++) {
-						weights_and_displacements_[i] = 0;
-					}
-					for (short j = 8; j < 12; j++) {
-						weights_and_displacements_[j] = 0.5;
-					}
-				}
-				else {
-					weights_and_displacements_ = weights_and_displacements;
-				}
-
-				this->weight_ForgetGate_for_HiddenState = weights_and_displacements_[0];
-				this->weight_InputGate_for_HiddenState = weights_and_displacements_[1];
-				this->weight_CandidateGate_for_HiddenState = weights_and_displacements_[2];
-				this->weight_OutputGate_for_HiddenState = weights_and_displacements_[3];
-
-				this->weight_ForgetGate_for_InputState = weights_and_displacements_[4];
-				this->weight_InputGate_for_InputState = weights_and_displacements_[5];
-				this->weight_CandidateGate_for_InputState = weights_and_displacements_[6];
-				this->weight_OutputGate_for_InputState = weights_and_displacements_[7];
-
-				this->displacement_ForgetState = weights_and_displacements_[8];
-				this->displacement_InputState = weights_and_displacements_[9];
-				this->displacement_CandidateState = weights_and_displacements_[10];
-				this->displacement_OutputState = weights_and_displacements_[11];
+			else {
+				weights_ = weights;
 			}
-		}
-		weights_and_displacement_Neuron() = default;
-	};
-	SimpleSNT(size_t NumberNeurons_ = 1, const std::vector <weights_and_displacement_Neuron> weights_and_displacements = { { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} } }) {
-		std::vector <weights_and_displacement_Neuron> weights_and_displacements_ = weights_and_displacements;
-		if (NumberNeurons_ > weights_and_displacements.size()) {
-			weights_and_displacements_.resize(NumberNeurons_);
-			for (size_t i = weights_and_displacements.size() - 1; i < NumberNeurons_; i++) {
-				weights_and_displacements_[i] = { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} };
-			}
-		}
-		else if (NumberNeurons_ < weights_and_displacements.size()) {
-			weights_and_displacements_.resize(NumberNeurons_);
-		}
-		this->Weights_and_Displacements_ = std::move(weights_and_displacements_);
 
-		this->NumberNeurons = NumberNeurons_;
-		this->OutputNeurons.resize(NumberNeurons, 0.0);
-		this->InputNeurons.resize(NumberNeurons, 0.0);
-		this->Hidden_state.resize(NumberNeurons, 0.0);
+			std::vector <long double> displacements_ = displacements;
+			if (displacements.size() < 4) {
+				displacements_ = displacements;
+				displacements_.resize(4);
+				for (short i = displacements.size() - 1; i < 4; i++) {
+					displacements_[i] = 0.5;
+				}
+			}
+			else {
+				displacements_ = displacements;
+			}
+
+			this->weight_ForgetGate_for_HiddenState = weights_[0];
+			this->weight_InputGate_for_HiddenState = weights_[1];
+			this->weight_CandidateGate_for_HiddenState = weights_[2];
+			this->weight_OutputGate_for_HiddenState = weights_[3];
+
+			this->weight_ForgetGate_for_InputState = weights_[4];
+			this->weight_InputGate_for_InputState = weights_[5];
+			this->weight_CandidateGate_for_InputState = weights_[6];
+			this->weight_OutputGate_for_InputState = weights_[7];
+
+			this->displacement_ForgetState = displacements_[0];
+			this->displacement_InputState = displacements_[1];
+			this->displacement_CandidateState = displacements_[2];
+			this->displacement_OutputState = displacements_[3];
+
+			this->NumberNeurons = NumberNeurons_;
+			this->OutputNeurons.resize(NumberNeurons, 0.0);
+			this->InputNeurons.resize(NumberNeurons, 0.0);
+			this->Hidden_state.resize(NumberNeurons, 0.0);
+		}
 	}
-	SimpleSNT(const std::vector <long double> InputNeurons_, const std::vector <weights_and_displacement_Neuron> weights_and_displacements = { { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} } }) {
-		std::vector <weights_and_displacement_Neuron> weights_and_displacements_ = weights_and_displacements;
-		if (InputNeurons_.size() > weights_and_displacements.size()) {
-			weights_and_displacements_.resize(InputNeurons_.size());
-			for (size_t i = weights_and_displacements.size() - 1; i < InputNeurons_.size(); i++) {
-				weights_and_displacements_[i] = { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} };
+	SimpleSNT(const std::vector <long double> InputNeurons_, const std::vector <long double> weights, const std::vector <long double> displacements) {
+		if (weights.size() > 0 && displacements.size() > 0) {
+			std::vector <long double> weights_ = weights;
+			if (weights.size() < 8) {
+				weights_ = weights;
+				weights_.resize(8);
+				for (short i = weights.size() - 1; i < 8; i++) {
+					weights_[i] = 0;
+				}
 			}
-		}
-		else if (InputNeurons_.size() < weights_and_displacements.size()) {
-			weights_and_displacements_.resize(InputNeurons_.size());
-		}
+			else {
+				weights_ = weights;
+			}
 
-		this->Weights_and_Displacements_ = std::move(weights_and_displacements_);
+			std::vector <long double> displacements_ = displacements;
+			if (displacements.size() < 4) {
+				displacements_ = displacements;
+				displacements_.resize(4);
+				for (short i = displacements.size() - 1; i < 4; i++) {
+					displacements_[i] = 0.5;
+				}
+			}
+			else {
+				displacements_ = displacements;
+			}
 
-		this->NumberNeurons = InputNeurons_.size();
-		this->InputNeurons = InputNeurons_;
-		this->OutputNeurons.resize(NumberNeurons, 0.0);
-		this->Hidden_state.resize(NumberNeurons, 0.0);
+			this->weight_ForgetGate_for_HiddenState = weights_[0];
+			this->weight_InputGate_for_HiddenState = weights_[1];
+			this->weight_CandidateGate_for_HiddenState = weights_[2];
+			this->weight_OutputGate_for_HiddenState = weights_[3];
+
+			this->weight_ForgetGate_for_InputState = weights_[4];
+			this->weight_InputGate_for_InputState = weights_[5];
+			this->weight_CandidateGate_for_InputState = weights_[6];
+			this->weight_OutputGate_for_InputState = weights_[7];
+
+			this->displacement_ForgetState = displacements_[0];
+			this->displacement_InputState = displacements_[1];
+			this->displacement_CandidateState = displacements_[2];
+			this->displacement_OutputState = displacements_[3];
+
+			this->NumberNeurons = InputNeurons_.size();
+			this->InputNeurons = InputNeurons_;
+			this->OutputNeurons.resize(NumberNeurons, 0.0);
+			this->Hidden_state.resize(NumberNeurons, 0.0);
+		}
 	}
-	SimpleSNT(std::vector <long double>&& InputNeurons_, const std::vector <weights_and_displacement_Neuron> weights_and_displacements = { { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} } }) {
-		std::vector <weights_and_displacement_Neuron> weights_and_displacements_ = weights_and_displacements;
-		if (InputNeurons_.size() > weights_and_displacements.size()) {
-			weights_and_displacements_.resize(InputNeurons_.size());
-			for (size_t i = weights_and_displacements.size() - 1; i < InputNeurons_.size(); i++) {
-				weights_and_displacements_[i] = { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} };
+	SimpleSNT(std::vector <long double>&& InputNeurons_, const std::vector <long double> weights, const std::vector <long double> displacements) {
+		if (weights.size() > 0 && displacements.size() > 0) {
+			std::vector <long double> weights_ = weights;
+			if (weights.size() < 8) {
+				weights_ = weights;
+				weights_.resize(8);
+				for (short i = weights.size() - 1; i < 8; i++) {
+					weights_[i] = 0;
+				}
 			}
-		}
-		else if (InputNeurons_.size() < weights_and_displacements.size()) {
-			weights_and_displacements_.resize(InputNeurons_.size());
-		}
+			else {
+				weights_ = weights;
+			}
 
-		this->Weights_and_Displacements_ = std::move(weights_and_displacements_);
+			std::vector <long double> displacements_ = displacements;
+			if (displacements.size() < 4) {
+				displacements_ = displacements;
+				displacements_.resize(4);
+				for (short i = displacements.size() - 1; i < 4; i++) {
+					displacements_[i] = 0.5;
+				}
+			}
+			else {
+				displacements_ = displacements;
+			}
 
-		this->NumberNeurons = InputNeurons_.size();
-		this->InputNeurons = std::move(InputNeurons_);
-		this->OutputNeurons.resize(NumberNeurons, 0.0);
-		this->Hidden_state.resize(NumberNeurons, 0.0);
+			this->weight_ForgetGate_for_HiddenState = weights_[0];
+			this->weight_InputGate_for_HiddenState = weights_[1];
+			this->weight_CandidateGate_for_HiddenState = weights_[2];
+			this->weight_OutputGate_for_HiddenState = weights_[3];
+
+			this->weight_ForgetGate_for_InputState = weights_[4];
+			this->weight_InputGate_for_InputState = weights_[5];
+			this->weight_CandidateGate_for_InputState = weights_[6];
+			this->weight_OutputGate_for_InputState = weights_[7];
+
+			this->displacement_ForgetState = displacements_[0];
+			this->displacement_InputState = displacements_[1];
+			this->displacement_CandidateState = displacements_[2];
+			this->displacement_OutputState = displacements_[3];
+
+			this->NumberNeurons = InputNeurons_.size();
+			this->InputNeurons = std::move(InputNeurons_);
+			this->OutputNeurons.resize(NumberNeurons, 0.0);
+			this->Hidden_state.resize(NumberNeurons, 0.0);
+		}
 	}
 	~SimpleSNT() = default;
-	void SetInputNeurons(const std::vector<long double> InputNeurons_, const std::vector <weights_and_displacement_Neuron> weights_and_displacements = { { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} } }) {
-		std::vector <weights_and_displacement_Neuron> weights_and_displacements_ = weights_and_displacements;
-		if (InputNeurons_.size() > weights_and_displacements.size()) {
-			weights_and_displacements_.resize(InputNeurons_.size());
-			for (size_t i = weights_and_displacements.size() - 1; i < InputNeurons_.size(); i++) {
-				weights_and_displacements_[i] = { { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }, {0.5, 0.5, 0.5, 0.5} };
-			}
-		}
-		else if (InputNeurons_.size() < weights_and_displacements.size()) {
-			weights_and_displacements_.resize(InputNeurons_.size());
-		}
-
-		this->Weights_and_Displacements_ = std::move(weights_and_displacements_);
+	void SetInputNeurons(const std::vector<long double> InputNeurons_) {
 		this->InputNeurons = InputNeurons_;	
 	}
+	void SetWeights(const std::vector <long double> weights) {
+		std::vector <long double> weights_ = weights;
+		if (weights.size() < 8) {
+			weights_ = weights;
+			weights_.resize(8);
+			for (short i = weights.size() - 1; i < 8; i++) {
+				weights_[i] = 0;
+			}
+		}
+		else {
+			weights_ = weights;
+		}
+		this->weight_ForgetGate_for_HiddenState = weights_[0];
+		this->weight_InputGate_for_HiddenState = weights_[1];
+		this->weight_CandidateGate_for_HiddenState = weights_[2];
+		this->weight_OutputGate_for_HiddenState = weights_[3];
 
+		this->weight_ForgetGate_for_InputState = weights_[4];
+		this->weight_InputGate_for_InputState = weights_[5];
+		this->weight_CandidateGate_for_InputState = weights_[6];
+		this->weight_OutputGate_for_InputState = weights_[7];
+	}
+	void SetDisplacements(const std::vector <long double> displacements) {
+		std::vector <long double> displacements_ = displacements;
+		if (displacements.size() < 4) {
+			displacements_ = displacements;
+			displacements_.resize(4);
+			for (short i = displacements.size() - 1; i < 4; i++) {
+				displacements_[i] = 0.5;
+			}
+		}
+		else {
+			displacements_ = displacements;
+		}
+
+		this->displacement_ForgetState = displacements_[0];
+		this->displacement_InputState = displacements_[1];
+		this->displacement_CandidateState = displacements_[2];
+		this->displacement_OutputState = displacements_[3];
+	}
+	void SetAll(const std::vector <long double> InputNeurons_, const std::vector <long double> weights, const std::vector <long double> displacements) {
+		SetInputNeurons(InputNeurons_);
+		SetWeights(weights);
+		SetDisplacements(displacements);
+	}
 	void CalculationAllNeurons() {
 		for (size_t i = 0; i < this->InputNeurons.size(); i++) {
 			neuron_state—alculation(i);
@@ -221,28 +257,30 @@ private:
 	std::vector <long double> InputNeurons;
 	std::vector <long double> Hidden_state;
 	std::vector <long double> OutputNeurons;
-	std::vector <weights_and_displacement_Neuron> Weights_and_Displacements_;
+	long double weight_ForgetGate_for_HiddenState, weight_InputGate_for_HiddenState, weight_CandidateGate_for_HiddenState, weight_OutputGate_for_HiddenState;
+	long double weight_ForgetGate_for_InputState, weight_InputGate_for_InputState, weight_CandidateGate_for_InputState, weight_OutputGate_for_InputState;
+	long double displacement_ForgetState, displacement_InputState, displacement_CandidateState, displacement_OutputState;
 
-	std::vector <long double> neuron—alculation(long double Hidden_State, long double Last_State, long double Input_State, weights_and_displacement_Neuron weights_and_displacement) {
+	std::vector <long double> neuron—alculation(long double Hidden_State, long double Last_State, long double Input_State) {
 		// Forget Gate: Â¯‡ÂÏ, ˜ÚÓ Á‡·˚Ú¸
 		long double ForgetGate = FunctionsAtivate::Sigmoid(
-			weights_and_displacement.weight_ForgetGate_for_HiddenState * Hidden_State
-			+ weights_and_displacement.weight_ForgetGate_for_InputState * Input_State
-			+ weights_and_displacement.displacement_ForgetState
+			this->weight_ForgetGate_for_HiddenState * Hidden_State
+			+ this->weight_ForgetGate_for_InputState * Input_State
+			+ this->displacement_ForgetState
 		);
 
 		// Input Gate: Â¯‡ÂÏ, ˜ÚÓ Ó·ÌÓ‚ËÚ¸
 		long double InputGate = FunctionsAtivate::Sigmoid(
-			weights_and_displacement.weight_InputGate_for_HiddenState * Hidden_State
-			+ weights_and_displacement.weight_InputGate_for_InputState * Input_State 
-			+ weights_and_displacement.displacement_InputState
+			this->weight_InputGate_for_HiddenState * Hidden_State
+			+ this->weight_InputGate_for_InputState * Input_State 
+			+ this->displacement_InputState
 		);
 
 		// ÕÓ‚˚È Í‡Ì‰Ë‰‡Ú ‰Îˇ ˇ˜ÂÈÍË
 		long double Ct_candidate = FunctionsAtivate::Tanh(
-			weights_and_displacement.weight_CandidateGate_for_HiddenState * Hidden_State
-			+ weights_and_displacement.weight_CandidateGate_for_InputState * Input_State
-			+ weights_and_displacement.displacement_CandidateState
+			this->weight_CandidateGate_for_HiddenState * Hidden_State
+			+ this->weight_CandidateGate_for_InputState * Input_State
+			+ this->displacement_CandidateState
 		);
 
 		// Œ·ÌÓ‚ÎˇÂÏ ÒÓÒÚÓˇÌËÂ ˇ˜ÂÈÍË
@@ -250,9 +288,9 @@ private:
 
 		// Output Gate: Â¯‡ÂÏ, ˜ÚÓ ÔÂÂ‰‡Ú¸ ‚ hidden_state
 		long double OutputGate = FunctionsAtivate::Sigmoid(
-			weights_and_displacement.weight_OutputGate_for_HiddenState * Hidden_State
-			+ weights_and_displacement.weight_OutputGate_for_InputState * Input_State
-			+ weights_and_displacement.displacement_OutputState
+			this->weight_OutputGate_for_HiddenState * Hidden_State
+			+ this->weight_OutputGate_for_InputState * Input_State
+			+ this->displacement_OutputState
 		);
 
 		// ÕÓ‚ÓÂ ÒÍ˚ÚÓÂ ÒÓÒÚÓˇÌËÂ
@@ -263,12 +301,12 @@ private:
 
 	void neuron_state—alculation(size_t number) {
 		if (number == 0) {
-			auto vec_ = std::move(neuron—alculation(0, 0, this->InputNeurons[number], this->Weights_and_Displacements_[number]));
+			auto vec_ = std::move(neuron—alculation(0, 0, this->InputNeurons[number]));
 			this->OutputNeurons[number] = std::move(vec_[0]);
 			this->Hidden_state[number] = std::move(vec_[1]);
 		}
 		else{
-			auto vec_ = std::move(neuron—alculation(this->Hidden_state[number - 1], this->OutputNeurons[number - 1], this->InputNeurons[number], this->Weights_and_Displacements_[number]));
+			auto vec_ = std::move(neuron—alculation(this->Hidden_state[number - 1], this->OutputNeurons[number - 1], this->InputNeurons[number]));
 			this->OutputNeurons[number] = std::move(vec_[0]);
 			this->Hidden_state[number] = std::move(vec_[1]);
 		}
